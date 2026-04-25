228288f28a0b13c917426c3e53cd48037342e0eb
Karys <118210782+K4rys@users.noreply.github.com>
2026-04-25 14:54:10 +0200

    Update fmt.Println output from 'Hello' to 'Goodbye'

--- bot.py
+++ bot.py
@@ -95,7 +95,7 @@ def get_active_challenge():
 # --- Constantes des salons ---
 MAKEUP_CHANNEL_NAME = "makeups"
 REPORT_CHANNEL_NAME = "botlxp"
-ANNOUNCE_CHANNEL_ID = 1380938525599338506     # ID de ton salon #annonces
+ANNOUNCE_CHANNEL_ID = 1380938525599338506    # ID de ton salon #annonces
 ANNOUNCE_MENTION = "@everyone"  # exemple : <@&123456789012345678>         # ou "@everyone", "@here", "<@&ID_ROLE>"
 
 # --- Vérification d'image ---
